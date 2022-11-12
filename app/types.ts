c210c71a02229d363c7c76e18a0093d551d480bf
Eric Smith <esmith11235@gmail.com>
2022-11-12 08:40:35 +0800

    feat(Balance/TransactionList): add types

---
+++ app/types.ts
@@ -0,0 +1,52 @@
+export interface Transaction {
+	createdAt: Date;
+	amount: number;
+	currency: Currency;
+	type: Type;
+	direction: Direction | null;
+	from?: From;
+	to?: To;
+}
+
+enum Currency {
+	BTC = "BTC",
+	CAD = "CAD",
+	Eth = "ETH",
+}
+
+enum Direction {
+	CREDIT = "credit",
+	DEBIT = "debit",
+}
+
+enum Type {
+	CONVERSION = "conversion",
+	EXTERNAL_ACCOUNT = "external account",
+	PEER = "peer",
+}
+
+interface From {
+	currency?: Currency;
+	amount?: number;
+}
+
+interface To {
+	toAddress?: string;
+	currency?: Currency;
+	amount?: number;
+}
+
+export interface Rates {
+	CAD_BTC: number;
+	BTC_CAD: number;
+	CAD_ETH: number;
+	ETH_CAD: number;
+	USD_BTC: number;
+	BTC_USD: number;
+	USD_ETH: number;
+	ETH_USD: number;
+	BTC_ETH: number;
+	ETH_BTC: number;
+	CAD_USD: number;
+	USD_CAD: number;
+}
