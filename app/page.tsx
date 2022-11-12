5f6f8fcd5b053624dec3e810f05608678e392d95
Eric Smith <esmith11235@gmail.com>
2022-11-12 15:16:39 +0800

    feat(Balance/TransactionList): calculate account balances

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,31 @@ export default async function Home() {
 		res.json()
 	);
 
+	const accountBalances = transactionHistory.reduce(
+		(prevBalance, currentTransaction) => {
+			if (!!currentTransaction.direction) {
+				const transactionTotal =
+					currentTransaction.direction === "credit" ? currentTransaction.amount : -currentTransaction.amount;
+
+				return {
+					...prevBalance,
+					[currentTransaction.currency]: prevBalance[currentTransaction.currency] + transactionTotal,
+				};
+			}
+
+			return prevBalance;
+		},
+		{ BTC: 0, ETH: 0, CAD: 0 }
+	);
+
+	const accountBalanceInCAD =
+		accountBalances.CAD + accountBalances.BTC * rates.BTC_CAD + accountBalances.ETH + rates.ETH_CAD;
+
 	return (
 		<div className="max-w-lg mx-auto absolute inset-0 divide-y divide-gray-700 bg-blue-100">
 			<div className="h-1/6">Header</div>
 			<div className="h-5/6">
-				{/* <div>{JSON.stringify(rates)}</div> */}
+				<div>{JSON.stringify(h)}</div>
 				<div className="overflow-y-scroll max-h-full">
 					{[1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 312, 32, 52, 412, 3, 124, 2354].map((v, i) => (
 						<div className="h-16 bg-green-400" key={i}>
