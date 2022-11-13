6ccd2699efc443c472cc922902a034edd71af69a
Eric Smith <esmith11235@gmail.com>
2022-11-13 16:16:49 +0800

    feat(Balance/TransactionList): add transaction details modal

--- app/transactions/[asset]/page.tsx
+++ app/transactions/[asset]/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { Container, ContainerHeader, ContainerScrollableBody } from "../../components/Container";
 import { AssetName, Currency, Direction, Transaction, Type } from "../../types";
-import { fetchTransactionHistory, fetchRates, balancesFromTransactionHistory } from "../../utils";
+import { fetchTransactionHistory, balancesFromTransactionHistory } from "../../utils";
+import { TransactionItem } from "../../components/TransactionItem";
 
 const GoHomeButton = () => {
 	return (
@@ -30,18 +31,6 @@ export default async function Page({ params }: { params: { asset: string } }) {
 
 	const accountBalances = balancesFromTransactionHistory(transactionHistory);
 
-	const imageForTransactionType = (transaction: Transaction): string => {
-		if (transaction.type === Type.CONVERSION) {
-			return "exchange.svg";
-		}
-
-		if (transaction.direction === Direction.CREDIT) {
-			return "cashin.svg";
-		}
-
-		return "cashout.svg";
-	};
-
 	return (
 		<Container>
 			<ContainerHeader>
@@ -62,40 +51,7 @@ export default async function Page({ params }: { params: { asset: string } }) {
 			<ContainerScrollableBody>
 				<div className="font-bold text-lg text-gray-800 p-4">Transactions</div>
 				{transactionHistory.map((transaction) => (
-					<div key={transaction.createdAt as any} className="h-20 flex items-center justify-between p-4">
-						<div className="flex items-center">
-							<Image
-								className="mr-4"
-								src={`/images/${imageForTransactionType(transaction)}`}
-								width={30}
-								height={30}
-								alt="Your Name"
-							/>
-							<div>
-								<div className="font-semibold">
-									{transaction.type +
-										(transaction.type === Type.CONVERSION
-											? ` ${transaction.from?.currency} -> ${transaction.to?.currency}`
-											: "")}
-								</div>
-								<span className="text-gray-600">
-									{new Date(transaction.createdAt).toLocaleDateString("en-us", {
-										year: "numeric",
-										month: "short",
-										day: "numeric",
-									})}
-								</span>
-							</div>
-						</div>
-						<div
-							className={`font-semibold text-md ${
-								transaction.direction === "credit" ? `text-green-500` : transaction.direction === "debit" ? "" : ""
-							}`}
-						>
-							{transaction.direction === "credit" ? "" : transaction.direction === "debit" ? "-" : ""}
-							{transaction.amount.toFixed(4)} {transaction.currency}
-						</div>
-					</div>
+					<TransactionItem transaction={transaction} key={transaction.createdAt as any} />
 				))}
 			</ContainerScrollableBody>
 		</Container>
